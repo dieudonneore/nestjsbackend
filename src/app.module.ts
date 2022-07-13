0ab3d35f5f28ccf065de35e50ced99b50ea8f820
dore <dore772@gmail.com>
2022-07-13 16:21:00 +0000

    update app.module.ts to add ContinentSchema to import section

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,12 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ContinentSchema } from './schemas/continent.schema';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017', {
-      dbName: 'ContinentDATABASE',
-    }),
+    MongooseModule.forRoot('mongodb://localhost:27017/ContinentDATABASE'),
+    MongooseModule.forFeature([{ name: 'Continent', schema: ContinentSchema }]),
   ],
   controllers: [AppController],
   providers: [AppService],
