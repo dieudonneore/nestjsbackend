5e6f96d9e1b766ef44996de916443c0e8bfcfa7b
dore <dore772@gmail.com>
2022-07-13 17:16:38 +0000

    update ContinentService with constructor

--- src/continent/continent.service.ts
+++ src/continent/continent.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { IContinent } from 'src/interface/continent.interface';
 
 @Injectable()
-export class ContinentService {}
+export class ContinentService {
+  constructor(
+    @InjectModel('Continent')
+    private continentModel: Model<IContinent>,
+  ) {}
+}
