d74739de63a4ed29d0115634d11375cc5ea9ee8d
dore <dore772@gmail.com>
2022-07-13 17:29:32 +0000

    add create and update features for continent

--- src/continent/continent.service.ts
+++ src/continent/continent.service.ts
@@ -1,6 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { ContinentDto } from 'src/dto/continent.dto';
+import { UpdateContinentDto } from 'src/dto/update-continent.dto';
 import { IContinent } from 'src/interface/continent.interface';
 
 @Injectable()
@@ -9,4 +11,24 @@ export class ContinentService {
     @InjectModel('Continent')
     private continentModel: Model<IContinent>,
   ) {}
+
+  async createContinent(continentDto: ContinentDto): Promise<IContinent> {
+    const newContinent = await new this.continentModel(continentDto);
+    return newContinent.save();
+  }
+
+  async updateStudent(
+    continentId: string,
+    updateContinentDto: UpdateContinentDto,
+  ): Promise<IContinent> {
+    const existingContinent = await this.continentModel.findByIdAndUpdate(
+      continentId,
+      updateContinentDto,
+      { new: true },
+    );
+    if (!existingContinent) {
+      throw new NotFoundException(`Student #${continentId} not found`);
+    }
+    return existingContinent;
+  }
 }
